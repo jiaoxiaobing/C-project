1c4e342b0cc833d3f03c4f8f0976d26071252eb9
jiaoxiaobing <32411175+jiaoxiaobing@users.noreply.github.com>
2017-10-30 00:34:58 +0800

    Update TESTTEXT.C

--- TESTTEXT.C
+++ TESTTEXT.C
@@ -2,7 +2,7 @@
  * mystart.c
  *
  *  Created on: 2017年10月26日
- *      Author: jiaobing
+ *      Author: jiaoxiaobing
  */
 	#include <stdio.h>
 	#include <dirent.h>
@@ -10,17 +10,25 @@
 	#include <stdlib.h>
 	#include <unistd.h>
 	#include <sys/stat.h>
+
 void cutthetext(char *path,char *path2)
 {
 	int z=0,j,y,i,h,st=0;
-	FILE *pfread,*pfwrite;;
+	FILE *pfread,*pfwrite;
 	char s[1024],a[1024];
-	pfread = fopen(path,"r+");
+	if ((pfread = fopen(path,"r+")) == NULL)
+	{
+
+		printf("%s\n","the path is error");
+		sleep(10);
+	}
+
 	pfwrite= fopen(path2,"w+");
 	while((fgets(s,1024,pfread))!=NULL)
 	{	printf("%s",s);
+
 			  z=0;
-			for(i=0;i<150;i++)
+			for(i=0;i<1024;i++)
 			{
 			   printf("%s\n","1111111111");
 				if(s[i] == ':')
@@ -59,7 +67,7 @@ void cutthetext(char *path,char *path2)
 
 			if(st==9)
 			{
-			 memset(a,0,1024);
+			 strcpy(a," ");
 			 printf("%s\n","wepirwermd122555");
 			   st=0;
 			}
@@ -71,21 +79,22 @@ void cutthetext(char *path,char *path2)
 
 }
 typedef struct{
-	 char DTS[20];
-	 char ddesc[200];
-	 char people[30];
-	char fileld[15];
-	 char de[20];
-	 char linre[3];
-	 char serv[4];
-	 char anysl[200];
+	 char DTS[1024];
+	 char ddesc[1024];
+	 char people[1024];
+	char fileld[1024];
+	 char de[1024];
+	 char linre[1024];
+	 char serv[1024];
+	 char anysl[1024];
 }dts1;
 void removeCRchar( char *p)
 {
-  int i =0;
+  int   i =0;
   while((*(p+i))!= '\r')
-  i++;
- (*(p+i)) = '\0';
+  {i++;
+  }
+ *(p+i) = '\0';
 
 }
 
@@ -94,18 +103,39 @@ void sort(char *path)
 {
 	dts1 dts;
 	//dts1 *d;
+	char *p;
 	int i,len,j;
 	FILE *fread,*fwrite;
-	char s[1024],a[1]="\r";
+	char s[1024];
+	  p=s;
 	char path3[40]="D:/Cadence/test/sort.txt";
 	fread = fopen(path,"r+");
 	fwrite = fopen(path3,"w+");
-	while((fgets(s,1024,fread))!=NULL)
+
+	// fpos_t home;
+	while( p !=NULL)
 	{
-		  removeCRchar(s);
+
 	for(i=0;i<=7;i++)
 	{
 	 // fgets(s,1024,fread);
+	p= fgets(s,1024,fread);
+		// removeCRchar(s);
+	int j=0;
+	for(j=0;j<1024;j++)
+	{
+		if (s[j]== '\r')
+		{
+
+			s[j]='\0';
+			break;
+		}
+
+	}
+	/*while(s[j]!='\r')
+		j++;
+	s[j]='\0';*/
+		 printf("%s",s);
 
 	  switch (i) {
 	    case 0 : strcpy(dts.DTS,s);   break;
@@ -114,14 +144,17 @@ void sort(char *path)
 	    case 3 : strcpy(dts.fileld,s);   break;
 	    case 4 : strcpy(dts.de,s);    break;
 	    case 5 : strcpy(dts.linre,s);   break;
-	    case 6 : strcpy(dts.serv,s);   break;
-	    case 7 : strcpy(dts.anysl,s);   break;
+	   case 6 : strcpy(dts.serv,s);   break;
+	 case 7 : strcpy(dts.anysl,s);   break;
 	  }
 
 	}
+
 	fputs(dts.DTS,fwrite);
 	fputs("     ",fwrite);
 	fputs(dts.ddesc,fwrite);
+	fputs("     ",fwrite);
+	fputs(dts.people,fwrite);
 	fputs("     ",fwrite);
 	fputs(dts.fileld,fwrite);
 	fputs("     ",fwrite);
@@ -130,8 +163,9 @@ void sort(char *path)
 	fputs(dts.linre,fwrite);
 	fputs("  ",fwrite);
 	fputs(dts.serv,fwrite);
-	fputs("   ",fwrite);
+	fputs("    ",fwrite);
 	fputs(dts.anysl,fwrite);
+	fputc('\r',fwrite);
 	//fputs(a,fwrite);
 
 	}
@@ -142,10 +176,10 @@ void sort(char *path)
 }
 	void main(void)
 	{
-		char path[40]="D:/Cadence/test/dtstest.txt" ;
+		char path[40]="D:/Cadence/test/testtext.txt" ;
 		char path2[40]="D:/Cadence/test/dts.txt" ;
-		//cutthetext(path,path2);
-		sort(path2);
+	   cutthetext(path,path2);
+		 sort(path2);
  }
 
 
